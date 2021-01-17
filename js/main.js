b9313ec5dbe3444f2f62cdeb32b866e36a0e983c
Allyson Reis <allysonreis1@gmail.com>
2021-01-17 20:49:12 -0300

    Implementacao do preloader

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 var btnContact = document.querySelector('.jl-btn-contact');
 
+window.addEventListener('load', function(){
+  var pagePreLoader = document.querySelector('.jl-preloader');
+
+    pagePreLoader.classList.add('jl-fade-out');
+ 
+    setTimeout(function () {
+        pagePreLoader.style.display = 'none';
+    }, 2000);
+})
+
 btnContact.addEventListener('click', function(){
   var boxContact = document.querySelector('.jl-contact-info');
-  boxContact.classList.toggle('jl-is-open')
+  boxContact.classList.toggle('jl-is-open');
 
   // Poderia ser usado 'this' no lugar de btnContact
   btnContact.classList.toggle('jl-change-icon');
