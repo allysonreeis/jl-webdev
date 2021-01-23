1807ca118c04d48b4e70d9578cb7639fe40492e7
Allyson Reis <allysonreis1@gmail.com>
2021-01-23 16:41:36 -0300

    Modal criado e animado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var btnContact = document.querySelector('.jl-btn-contact');
+var toggleModal = document.querySelectorAll('.jl-toggle-modal');
 
 window.addEventListener('load', function(){
   var pagePreLoader = document.querySelector('.jl-preloader');
@@ -17,3 +18,15 @@ btnContact.addEventListener('click', function(){
   // Poderia ser usado 'this' no lugar de btnContact
   btnContact.classList.toggle('jl-change-icon');
 });
+
+// Botão de orçamento
+for (let i = 0; i < toggleModal.length; i++) {
+  toggleModal[i].addEventListener('click', function(){
+    var overlay = document.querySelector('.jl-overlay');
+    var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+    
+    overlay.classList.toggle('jl-is-open');
+    modalOrcamento.classList.toggle('jl-is-open');
+    modalOrcamento.classList.toggle('jl-slide-top-in');
+  });
+}
