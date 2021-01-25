f32e833b83c610423bcdc03312960568061d9d6b
Allyson Reis <allysonreis1@gmail.com>
2021-01-25 18:36:10 -0300

    Animando o indicador de scroll com waypoints

--- js/main.js
+++ js/main.js
@@ -30,3 +30,14 @@ for (let i = 0; i < toggleModal.length; i++) {
     modalOrcamento.classList.toggle('jl-slide-top-in');
   });
 }
+
+// Scroll com Waypoints
+var myScrollDown = document.querySelector('.jl-scroll-down');
+
+var waypoint = new Waypoint({
+  element: myScrollDown,
+  handler: function() {
+    myScrollDown.classList.toggle('jl-fade-out')
+  },
+  offset: '80%'
+})
