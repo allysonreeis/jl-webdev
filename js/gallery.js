0cd1ede13df3d27c1dd72b16d3f4e58389f665f6
Allyson Reis <allysonreis1@gmail.com>
2021-03-06 12:14:33 -0300

    Adicionando e removendo o overlay com toggle (add e remove)

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+var overlay = document.querySelector('.jl-overlay');
+var frameImage = document.querySelector('.jl-gallery-frame-image');
+var frameContainer = document.querySelector('.jl-gallery-frame-container');
+var galleryImages = document.querySelectorAll('.jl-thumb-img');
+var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
+
+const getImageSrc = function(){
+  for (let i = 0; i < galleryImages.length; i++) {
+    galleryImages[i].addEventListener('click', function(){
+      var imageSrc = this.getAttribute('data-src');
+      frameImage.setAttribute('src', imageSrc);
+
+      overlay.classList.add('jl-is-open');
+      frameContainer.classList.add('jl-is-open');
+    })
+  }
+}
+
+for (let i = 0; i < closeGallery.length; i++) {
+  closeGallery[i].addEventListener('click', function(){
+      overlay.classList.remove('jl-is-open');
+      frameContainer.classList.remove('jl-is-open');
+  });
+}
+
+getImageSrc()
