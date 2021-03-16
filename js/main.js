b65e1dcbe292ad3a5fc7d127495082b4e793f0ca
Allyson Reis <allysonreis1@gmail.com>
2021-03-16 18:47:59 -0300

    Fazendo a responsividade do menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var btnContact = document.querySelector('.jl-btn-contact');
 var toggleModal = document.querySelectorAll('.jl-toggle-modal');
+var toggleMenu = document.querySelectorAll('.jl-toggle-menu');
+var menuMobile = document.querySelector('.jl-menu-mob');
+var btnMenuMobIcon = document.querySelector('.jl-btn-menu-mob ion-icon');
 
 window.addEventListener('load', function(){
   var pagePreLoader = document.querySelector('.jl-preloader');
@@ -19,11 +22,29 @@ btnContact.addEventListener('click', function(){
   btnContact.classList.toggle('jl-change-icon');
 });
 
+for (let i = 0; i < toggleMenu.length; i++) {
+  toggleMenu[i].addEventListener('click', function(){
+    var overlay = document.querySelector('.jl-menu-overlay');
+    
+    menuMobile.classList.toggle('jl-menu-is-closed');
+    menuMobile.classList.toggle('jl-menu-is-open');
+    overlay.classList.toggle('jl-is-open');
+    
+    var icon = btnMenuMobIcon.getAttribute('name');
+
+    if (icon == 'menu-outline') {
+      btnMenuMobIcon.setAttribute('name', 'close-outline');
+    } else {
+      btnMenuMobIcon.setAttribute('name', 'menu-outline');
+    }
+  });
+}
+
 // Botão de orçamento
 for (let i = 0; i < toggleModal.length; i++) {
   toggleModal[i].addEventListener('click', function(){
-    var overlay = document.querySelector('.jl-overlay');
     var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+    var overlay = document.querySelector('.jl-overlay');
     
     overlay.classList.toggle('jl-is-open');
     modalOrcamento.classList.toggle('jl-is-open');
